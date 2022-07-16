c7370891f314413acf004285c4cde193313166f6
aliniko <aliverchan@gmail.com>
2022-07-16 09:58:08 +0200

    toggle the buttons

---
+++ Navbar/app.js
@@ -0,0 +1,10 @@
+
+
+const navToggle = document.querySelector('.nav-toggle')
+const links =document.querySelector('.links')
+
+navToggle.addEventListener('click', function(){
+    // console.log(links.classList)
+    links.classList.toggle('show-links')
+
+})
