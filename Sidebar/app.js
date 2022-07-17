ab3d256fe7c6c6c25ae81d40f0b818d1750c4cbd
aliniko <aliverchan@gmail.com>
2022-07-17 12:57:29 +0200

    toggle and closed buttons

---
+++ Sidebar/app.js
@@ -0,0 +1,13 @@
+// calling classes
+
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+
+toggleBtn.addEventListener("click", function(){
+    sidebar.classList.toggle("show-sidebar");
+})
+
+closeBtn.addEventListener("click", function(){
+    sidebar.classList.remove("show-sidebar")
+})
