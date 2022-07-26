18a6d18857f427a358bfb1447fd61f927d0c440e
aliniko <aliverchan@gmail.com>
2022-07-26 10:27:16 +0200

    selector inside elements

---
+++ 04-Questions/app.js
@@ -0,0 +1,19 @@
+// Using selector inside the element
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function(question){
+//    console.log(question)
+
+    const btn = question.querySelector(".question-btn");
+// console.log(btn)
+
+    btn.addEventListener("click", function(){
+        questions.forEach(function(item){
+            if(item !== question){
+                item.classList.remove("show-text")
+            }
+        })
+        question.classList.toggle("show-text")
+    })
+})
