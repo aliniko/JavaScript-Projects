eb31b4889a766b6e579a28ff53f628406fb3c3ac
aliniko <aliverchan@gmail.com>
2022-08-19 09:07:46 +0200

    add and remove items

--- 11-Grocery-bud/app.js
+++ 11-Grocery-bud/app.js
@@ -1,11 +1,124 @@
 // ****** SELECT ITEMS **********
+const form = document.querySelector(".grocery-form");
+const alert = document.querySelector(".alert");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
 
 // edit option
-
+let editElement;
+let editFlag = false;
+let editID = "";
 // ****** EVENT LISTENERS **********
+// submit form
+form.addEventListener("submit", addItem);
+// clear item 
+clearBtn.addEventListener("click", clearItems)
 
 // ****** FUNCTIONS **********
 
+// add item
+function addItem(e){
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+
+    if(value !== "" && !editFlag){
+        const element = document.createElement("article");
+        let attr = document.createAttribute("data-id");
+        attr.value = id;
+        element.setAttributeNode(attr);
+        element.classList.add("grocery-item");
+        element.innerHTML = `<p class="title"> ${value}</p>
+        <div class="btn-container">
+          <!-- edit btn -->
+          <button type="button" class="edit-btn">
+            <i class="fas fa-edit"></i>
+          </button>
+          <!-- delete btn -->
+          <button type="button" class="delete-btn">
+            <i class="fas fa-trash"></i>
+          </button>
+        </div>`;
+
+        // add event listeners to both buttons;
+        const deleteBtn = element.querySelector(".delete-btn");
+        deleteBtn.addEventListener("click", deleteItem);
+        const editBtn = element.querySelector(".edit-btn");
+        editBtn.addEventListener("click", editItem);
+     
+      // append child
+      list.appendChild(element);
+      // display alert
+      displayAlert("item added to the list", "success");
+      // set local storage
+
+      // set back to default
+      setBackToDefault();
+    }else if( value !== "" && editFlag){
+      editElement.innerHTML = value;
+      displayAlert("value changed", "success");
+
+      // edit local storage
+
+    }else {
+        displayAlert("please enter value", "danger");
+    }
+}
+// display alert
+function displayAlert(text, action){
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+    // remove alert
+    setTimeout(function(){
+        alert.textContent = "";
+        alert.classList.remove(`alert-${action}`)
+    }, 1000);
+}
+
+// delte item
+
+function deleteItem(e){
+    const element = e.currentTarget.parentElement.parentElement;
+    const id = element.dataset.id;
+
+    list.removeChild(element);
+    if(list.children.length === 0){
+      container.classList.remove("show-container");
+    }
+    displayAlert("item removed", "danger");
+
+    setBackToDefault();
+    // remove from local storage
+
+}
+
+// clear item
+function clearItems(){
+  const items = document.querySelectorAll(".grocery-item");
+  if(items.length > 0){
+    items.forEach(function(item){
+      list.removeChild(item);
+    })
+  }
+  container.classList.remove("show-container");
+  displayAlert("empty list", "danger");
+  setBackToDefault();
+  localStorage.removeItem("list");
+}
+// edit item
+function editItem(e){
+
+}
+
+function setBackToDefault(){
+  grocery.value = "";
+  editFlag = false;
+  editID = "";
+  submitBtn.textContent = "submit";
+}
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
