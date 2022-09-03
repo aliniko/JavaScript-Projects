a4ed812c9a3d6fac9cf429abeb027da78a7c3227
aliniko <aliverchan@gmail.com>
2022-09-03 09:38:43 +0200

    dark mode added

--- 16-Dark_Mode/app.js
+++ 16-Dark_Mode/app.js
@@ -1,1 +1,10 @@
-console.log('dark mode');
+
+const toggleBtn = document.querySelector('.btn');
+const articlesContainer = document.querySelector('.articles');
+
+toggleBtn.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark-mode');
+
+// var element = document.body;
+//    element.classList.toggle("dark-mode");
+});
